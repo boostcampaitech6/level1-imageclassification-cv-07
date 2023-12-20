ba1fad46a099bac8a8ffc1c899d0f80ea21446a7
jhd <wlgusehd0@gmail.com>
2023-12-20 13:48:32 +0000

    mixup-adamw-focal-mobilevitv2

--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
-python src/train.py --config configs/train.yaml
-python src/multi_label_train.py --config configs/multi_label_train.yaml
+python src/eval.py --config configs/test.yaml
+# python src/train.py --config configs/train.yaml
+# python src/multi_label_train.py --config configs/multi_label_train.yaml
