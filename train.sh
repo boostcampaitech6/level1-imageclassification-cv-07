8f9b0a17b6b9366cc87da90fa760e7fb8a6c810e
jhd <wlgusehd0@gmail.com>
2023-12-21 01:35:17 +0000

    segmentation-efficientnet exp

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
-python src/eval.py --config configs/test.yaml
-python src/eval.py --config configs/test_seg.yaml
+# python src/eval.py --config configs/test.yaml
+# python src/eval.py --config configs/test_seg.yaml
 
 # python src/train.py --config configs/train.yaml
+# python src/train.py --config configs/train_seg.yaml
 # python src/multi_label_train.py --config configs/multi_label_train.yaml
